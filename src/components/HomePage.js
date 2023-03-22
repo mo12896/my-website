606a5d71c72f4fe5ee11d1a46189c96118c4ed82
@moritz2018.mueller <moritz2018.mueller@outlook.de>
2023-03-22 02:10:04 +0100

    Change the HomePage text

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -6,7 +6,7 @@ function HomePage() {
 
   useEffect(() => {
     const delay = 100; // set the delay in milliseconds
-    const words = "Welcome to my personal portfolio website! As a Software Engineer, I'm excited to showcase my skills, experiences, and projects that have shaped my career.".split(' ');
+    const words = "Welcome to my personal portfolio website! As a Software Engineer and AI Developer, I'm excited to showcase my skills, experiences, and projects that have shaped my career. This website serves as part of my portfolio, and I have implemented the frontend using React. The website has been deployed using Github and AWS Amplify, which allows me to easily update and showcase my projects. While the frontend is up and running, I'm still working on implementing the backend. Once completed, the website will be able to handle contact requests and other interactions. In the meantime, please don't hesitate to reach out to me via email at moritz1996.mueller@gmail.com. ".split(' ');
     let count = 0;
 
     const interval = setInterval(() => {
